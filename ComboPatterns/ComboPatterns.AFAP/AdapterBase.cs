461ff79ffc2508930580298aea2286fbc716f582
Zhuel Itua <ituazhuel@hotmail.com>
2019-09-21 15:41:01 +0300

    (dev-2)
    - create project ComboPatterns.AFAP

---
+++ ComboPatterns/ComboPatterns.AFAP/AdapterBase.cs
@@ -0,0 +1,42 @@
+﻿using ComboPatterns.Interfaces;
+using System;
+
+namespace ComboPatterns.AFAP
+{
+    /// <summary>
+    /// Implementation <see cref="IAdapter"/>
+    /// </summary>
+    public abstract class AdapterBase : IAdapter
+    {
+        protected private IAbstractFactory _factory;
+
+        /// <summary>
+        /// Adapter creation method
+        /// </summary>
+        /// <typeparam name="TAdapter">type of adapter created</typeparam>
+        /// <returns>adapter <typeparamref name="TAdapter"/></returns>
+        public virtual TAdapter GetAdapter<TAdapter>() where TAdapter : IAdapter, new() => GetAdapterStatic<TAdapter>(_factory);
+
+        /// <summary>
+        /// Adapter creation method
+        /// </summary>
+        /// <typeparam name="TAdapter">type of adapter created</typeparam>
+        /// <param name="factory">adapter factory</param>
+        /// <returns>adapter <typeparamref name="TAdapter"/></returns>
+        public static TAdapter GetAdapterStatic<TAdapter>(IAbstractFactory factory)
+             where TAdapter : IAdapter, new()
+        {
+            return factory.CreateObject<object, TAdapter>(
+                (_) =>
+                {
+                    var adapter = new TAdapter();
+
+                    if (adapter is AdapterBase adapterBase)
+                        adapterBase._factory = factory;
+
+                    return adapter;
+                },
+                null);
+        }
+    }
+}
