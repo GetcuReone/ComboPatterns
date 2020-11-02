cc470e18b84073206e48228eb4734bde275cc80f
Juel <GetcuReone@hotmail.com>
2020-11-03 01:16:02 +0300

    fix xml comments

--- ComboPatterns/Factory/ComboPatterns.Factory/FactoryBase.cs
+++ ComboPatterns/Factory/ComboPatterns.Factory/FactoryBase.cs
@@ -4,18 +4,18 @@ using System;
 namespace GetcuReone.ComboPatterns.Factory
 {
     /// <summary>
-    /// abstract object creation factory
+    /// abstract object creation factory.
     /// </summary>
     public class FactoryBase : IAbstractFactory
     {
         /// <summary>
-        /// Object Creation Method
+        /// Object creation method.
         /// </summary>
-        /// <typeparam name="TObj">type of object to create</typeparam>
-        /// <typeparam name="TParameter">type of parameters for creating an object</typeparam>
-        /// <param name="factoryFunc">function to create objects</param>
-        /// <param name="parameter">options for creating an object</param>
-        /// <exception cref="ArgumentNullException">if <paramref name="factoryFunc"/> is null</exception>
+        /// <typeparam name="TObj">type of object to create.</typeparam>
+        /// <typeparam name="TParameter">type of parameters for creating an object.</typeparam>
+        /// <param name="factoryFunc">function to create objects.</param>
+        /// <param name="parameter">options for creating an object.</param>
+        /// <exception cref="ArgumentNullException">if <paramref name="factoryFunc"/> is null.</exception>
         /// <returns>object</returns>
         public virtual TObj CreateObject<TParameter, TObj>(Func<TParameter, TObj> factoryFunc, TParameter parameter)
         {
